eb5494bcbb0dd48bd292a251bd941e8debf22fd1
Philipp Schöneberg <philipp.schoeneberg2004@gmx.de>
2023-08-23 18:59:30 +0200

    New map style && new hints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicGgxbDFwcCIsImEiOiJjbGxreXZkZ28xeW5xM2RwcXlrb
 
 var gameOver = false;
 var usedCounter;
+var hintCounter = 0;
 var mylng;
 var mylat;
 var options = {units: 'meters'}
@@ -22,14 +23,14 @@ var markerPlaced =  false;
 
 const mainPopup = document.querySelector(".mainPopup");
 const losGehtsButton = document.querySelector(".losGehtsButton");
-const tippButton = document.querySelector(".tippButton");
+const hintButton = document.querySelector(".hintButton");
 
 const endPopup = document.querySelector(".endPopup");
 const okButton = document.querySelector(".okButton");
 
 var places = [
-                ["Rathaus", "9.473782", "53.601507", "false", "Rathaustipp 1 ", "Rathaustipp 2"],
-                ["Zeughaus", "9.475159", "53.600133", "false", "Zeughaustipp 1 ", "Zeughaustipp 2"]
+                ["Rathaus", "9.4756788", "53.6023594", "false", "Das zwei Etagen hohe Bauwerk misst im Hauptgebäude eine Län-ge von 35 Metern und im Ostflügel eine Länge von 31 Metern . Bis zur Fertigstellung des direkt mit dem ursprünglichen Teil verbunde-nen Neubaus im Jahre 1988 war das in der Schwedenzeit errichtete Gebäude Mittelpunkt der Verwaltung Stades. ", "Jedoch befinden sich heute noch der Ratskeller, das Standesamt, der Ratssaal, eini-ge Büros und der nach dem schwedischen Grafen benannte Kö-nigsmarcksaal, der als Festsaal dient, in dem alten Gebäude. ", "Über dem Haupteingang zur Hökerstraße ist das Wappen von Schwedens König Karl XI abgebildet und erinnert ebenfalls an die Herrschaftszeit in Stade .", "resources/figures/rathaus0.jpg"],
+                ["Zeughaus", "9.4751193", "53.6004986", "false", "Das in den Jahren 1697-1699 errichtete Gebäude diente den Schweden im Krieg gegen die Dänen als Lagerplatz für Waffen und Material. ", "Der Name des Gebäudes steht groß über dem Haupteingang des zweistöckigen Großbaus. Über dem Portal ist das Wappen des schwedischen Königs, zu dieser Zeit Karl XII, abgebildet. ", "Seit dem Umbau zur Markthalle vor rund einhundert Jahren hatte das Gebäude viele Funktionen. So zogen in das Gebäude zeitweise ein Kino sowie eine Jugendherberge und heute eine Eisdiele und eine irische Bar.", "resources/figures/zeughaus0.jpg"]
              ]
 
 
@@ -59,7 +60,7 @@ function rand (min, max) {
     }
 }*/
 
-document.querySelector("#historicText").innerText = places[todaysPlace][4];
+document.querySelector("#historicImg").src=places[todaysPlace][7];
 
 map.on('click', (e) => {
     mylng = e.lngLat.lng;
@@ -95,9 +96,18 @@ mainButton.addEventListener("click", () => {
         gameOver = true;
         addMarker(places[todaysPlace][1], places[todaysPlace][2]);
         distance = Math.round(turf.distance([places[todaysPlace][1], places[todaysPlace][2]], [mylng, mylat], options));
-        points = Math.round(1/(distance+1)*1000);
+        if(distance <= 5){
+            points = 1000;
+        }
+        else if(distance < 500){
+            points = Math.round((1000-distance*2)*1/hintCounter+1);
+        }
+        else{
+            points = 0;
+        }
         document.querySelector("#endText").innerText = "Du warst " + distance + " Meter vom Ziel entfernt und erhälst " + points + " Punkte.";
         endPopup.classList.add("openPopup");
+        hintCounter = 0;
     }
 })
 
@@ -107,8 +117,20 @@ losGehtsButton.addEventListener("click", () => {
     document.querySelector(".mainButton").innerText = "Absenden";
 })
 
-tippButton.addEventListener("click", () => {
-    document.querySelector("#historicText").innerText = places[todaysPlace][4]+places[todaysPlace][5];
+hintButton.addEventListener("click", () => {
+    if(hintCounter == 0){
+        hintCounter++;
+        document.querySelector("#historicText").innerText = places[todaysPlace][4]
+    }
+    else if(hintCounter == 1){
+        hintCounter++;
+        document.querySelector("#historicText").innerText = document.querySelector("#historicText").innerText+places[todaysPlace][5];
+    }
+    else if(hintCounter == 2){
+        hintCounter++;
+        document.querySelector("#historicText").innerText = document.querySelector("#historicText").innerText+places[todaysPlace][6];
+        hintButton.classList.add("hide");
+    }
 })
 
 okButton.addEventListener("click", () => {
