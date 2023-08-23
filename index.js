1f313ee4b95e3ec383b18e9a661dce3a8bf9a79b
Philipp Schöneberg <philipp.schoeneberg2004@gmx.de>
2023-08-23 19:19:17 +0200

    Fixed losGehtsButton && calculation of points

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ mainButton.addEventListener("click", () => {
             points = 1000;
         }
         else if(distance < 500){
-            points = Math.round((1000-distance*2)*1/hintCounter+1);
+            points = Math.round((1000-distance*2)/(hintCounter+1));
         }
         else{
             points = 0;
